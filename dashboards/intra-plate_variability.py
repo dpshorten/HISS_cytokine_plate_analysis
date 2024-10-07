49f61a035bc3abfa75465727fbf4c6e5b4bc3e82
David Shorten <dpshorten@gmail.com>
2024-10-07 14:15:15 +1030

    updated intra-plate variability dashboard

--- dashboards/intra-plate_variability.py
+++ dashboards/intra-plate_variability.py
@@ -2,6 +2,7 @@ import sys
 import yaml
 import os
 import dash
+import plotly.graph_objects as go
 import numpy as np
 from dash import dcc, html, callback
 from dash.dependencies import Input, Output
@@ -10,14 +11,30 @@ import pandas as pd
 pd.options.mode.chained_assignment = None
 
 sys.path.append('../python/')
-from dashboard_notebook_util import read_estimated_concentrations, get_base_base_directory_path
+from dashboard_notebook_util import (
+    read_estimated_concentrations,
+    read_plate_data_with_calibration_concentrations,
+    read_quality_control_concentrations,
+    get_base_base_directory_path
+)
 from plotly_figure_parameters import dict_y_axis_parameters, dict_font_parameters, dict_x_axis_parameters_categorical
+from analysis_util import get_table_with_all_duplicate_qc_checks
+import plate_util
 
-
-dash.register_page(__name__, path='/duplicate_samples/')
+dash.register_page(__name__, path='/intra-plate_variability/')
 
 dict_parameters = yaml.safe_load(open("../parameters/july_2024_data_parameters.yaml", "r"))
 pd_df_estimated_concentrations = read_estimated_concentrations(dict_parameters)
+pd_df_plate_data = read_plate_data_with_calibration_concentrations(dict_parameters)
+pd_df_calibration_concentrations = plate_util.read_and_clean_calibration_concentrations(
+    dict_parameters,
+    str_base_directory_path=get_base_base_directory_path(dict_parameters)
+)
+pd_df_data = get_table_with_all_duplicate_qc_checks(
+    dict_parameters,
+    pd_df_estimated_concentrations,
+    pd_df_calibration_concentrations
+)
 
 pd_df_plate_data = pd.read_csv(
     open(
@@ -29,56 +46,6 @@ pd_df_plate_data = pd.read_csv(
         "rb"
     ),
     index_col=0,
-)
-
-def calculate_paired_intra_plate_cv(str_analyte, pd_group):
-    if len(pd_group) != 2:
-        return np.nan
-    estimate_1, estimate_2 = pd_group[f"estimated concentration {str_analyte}"].values
-    mean = (estimate_1 + estimate_2) / 2
-    std_dev = np.sqrt((estimate_1 - mean)**2 + (estimate_2 - mean)**2)
-    return std_dev / mean
-
-def calculate_paired_intra_plate_rel_abs_diff(str_analyte, pd_group):
-    if len(pd_group) != 2:
-        return np.nan
-    estimate_1, estimate_2 = pd_group[f"estimated concentration {str_analyte}"].values
-    mean = (estimate_1 + estimate_2) / 2
-    return np.abs(estimate_1 - estimate_2) / mean
-
-
-def get_table_of_duplicate_qc_checks(function_check, str_check_name):
-    list_pd_df_estimated_concentrations_checked_one_analyte = []
-    for str_analyte in dict_parameters["list of analytes"]:
-        pd_df_estimated_concentrations_checked_one_analyte = (
-            pd_df_estimated_concentrations
-            .groupby(["sample name annotations", "plate number"])
-            .apply(lambda x: function_check(str_analyte, x), include_groups=False)
-            .reset_index()
-        )
-        pd_df_estimated_concentrations_checked_one_analyte.columns = ["sample name annotations", "plate number"] + [
-            f"{str_check_name} {str_analyte}"]
-        list_pd_df_estimated_concentrations_checked_one_analyte.append(
-            pd_df_estimated_concentrations_checked_one_analyte)
-
-    pd_df_estimated_concentrations_checked = list_pd_df_estimated_concentrations_checked_one_analyte[0]
-    for pd_df_estimated_concentrations_checked_one_analyte in list_pd_df_estimated_concentrations_checked_one_analyte[
-                                                              1:]:
-        pd_df_estimated_concentrations_checked = pd.merge(
-            pd_df_estimated_concentrations_checked,
-            pd_df_estimated_concentrations_checked_one_analyte,
-            on=["sample name annotations", "plate number"],
-            how="outer"
-        )
-    return pd_df_estimated_concentrations_checked
-
-pd_df_CV = get_table_of_duplicate_qc_checks(
-    calculate_paired_intra_plate_cv,
-    "CV"
-)
-pd_df_rel_abs_diff = get_table_of_duplicate_qc_checks(
-    calculate_paired_intra_plate_rel_abs_diff,
-    "rel. abs. diff."
 )
 
 layout = html.Div([
@@ -117,7 +84,21 @@ layout = html.Div([
         ),
     ], style={'width': '20%', 'display': 'inline-block'}),
 
-    dcc.Graph(id='scatter-plot-duplicates')
+    html.Div([
+        html.Label("Strip colour:"),
+        dcc.Dropdown(
+            id='strip-colour-dropdown',
+            options=[
+                {'label': 'max gradient', 'value': 'max gradient'},
+                {'label': 'max cal interval', 'value': 'max cal interval'},
+            ],
+            value='max gradient',
+        ),
+    ], style={'width': '20%', 'display': 'inline-block'}),
+
+    html.Div([
+        dcc.Graph(id='scatter-plot-duplicates')
+    ], style={'width': '80%'}),
 ], style={'backgroundColor': 'white', 'padding': '20px'})
 
 
@@ -126,13 +107,9 @@ layout = html.Div([
     Input('analyte-dropdown', 'value'),
     Input('plot-type-dropdown', 'value'),
     Input('statistic-dropdown', 'value'),
+    Input('strip-colour-dropdown', 'value'),
 )
-def update_graph(str_analyte, str_plot_type, str_statistic):
-    if str_statistic == "CV":
-        pd_df_data = pd_df_CV
-    elif str_statistic == "rel. abs. diff.":
-        pd_df_data = pd_df_rel_abs_diff
-
+def update_graph(str_analyte, str_plot_type, str_statistic, str_strip_colour):
     str_column_name = f"{str_statistic} {str_analyte}"
 
     if str_plot_type == "box":
@@ -143,12 +120,23 @@ def update_graph(str_analyte, str_plot_type, str_statistic):
             hover_name="sample name annotations",
         )
     elif str_plot_type == "strip":
-        fig = px.strip(
-            pd_df_data,
-            x="plate number",
-            y=str_column_name,
-            hover_name="sample name annotations",
-        )
+
+        fig = go.Figure()
+
+        fig.add_trace(go.Scatter(
+            x=pd_df_data["plate number"] + np.random.uniform(-0.1, 0.1, len(pd_df_data)),
+            y=pd_df_data[str_column_name],
+            mode='markers',
+            marker=dict(
+                size=8,
+                color=pd_df_data[str_strip_colour + " " + str_analyte],
+                colorscale='Viridis',
+                showscale=True,
+                colorbar=dict(title="\n".join(str_strip_colour.split(" ")), titlefont=dict(size=14)),
+
+            ),
+            hovertext=pd_df_data["sample name annotations"],
+        ))
         fig.update_traces(marker=dict(opacity=0.75))
     if str_statistic == "CV":
         str_y_axis_title = "%CV"
@@ -160,6 +148,8 @@ def update_graph(str_analyte, str_plot_type, str_statistic):
         font=dict_font_parameters,
         xaxis_title="Plate",
         yaxis_title=str_y_axis_title,
+        plot_bgcolor='white',
+        paper_bgcolor='white',
     )
 
     return fig
